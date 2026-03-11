7f8d9504f783f81ebff3dfe1b279c64e4acb3fad
aditya6002 <adityaofficial690@gmail.com>
2026-03-11 13:26:39 +0530

    Add database connection utility and initial route files

---
+++ src/lib/db.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect():Promise<void> {
+    if(connection.isConnected){
+        console.log("Already Connected to database")
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI || '');
+        connection.isConnected = db.connections[0].readyState;
+        console.log("Database Connected Successfully");
+    } catch (error) {
+        console.log("Database Connection Error :- ", error);
+        process.exit(1);
+
+    }
+}
+
+
+export default dbConnect;
---
+++ src/routes/auth.route.ts
@@ -0,0 +1,1 @@
+import Modul from "module";
